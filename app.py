222603d56d55f754725746617bd6e703cdb410ad
Your Name <you@example.com>
2023-02-10 02:30:36 +0300

    updated version, support two flow

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ import re
 import time
 from dotenv import load_dotenv
 from cf import Cloudflare
-
 load_dotenv()
+
+SCAN_EVERY = int(os.environ.get('SCAN_EVERY')) if os.environ.get('SCAN_EVERY') else 100
+AVARAGE_COEFFICIENT = int(os.environ.get('AVARAGE_COEFFICIENT')) if os.environ.get('AVARAGE_COEFFICIENT') else 10
+RPS_MAX = int(os.environ.get('RPS_MAX')) if os.environ.get('RPS_MAX') else 40
 
 if not os.environ.get('CLOUDFLARE_API_KEY'):    
     print('CLOUDFLARE_API_KEY is not set')
@@ -15,14 +18,27 @@ if not os.environ.get('CLOUDFLARE_API_KEY'):
 
 cloudflare = Cloudflare(os.environ.get('CLOUDFLARE_API_KEY'))
 
-routes_to_exclude = [
-    'socket.io/'
-]
+lines_scanned = 0
+routes_to_exclude = os.environ.get('EXCLUDE_ROUTES').split(",") if os.environ.get('EXCLUDE_ROUTES') else []
 
-access_log = '/var/log/nginx/access.log'
-# Access log format (ipv4)
+# Flow number 1
+localCmd = (os.environ.get('LOCAL_LOGS') if os.environ.get('LOCAL_LOGS') else 'tail -f /var/log/nginx/access.log').split(" ")
+localLogs = None
+
+# Flow number 2
+# We can use this to get logs from remote server (or another file on local server)
+remoteCmd = (os.environ.get('REMOTE_LOGS').split(" ") if os.environ.get('REMOTE_LOGS') else None)
+remoteLogs = None
+
+# To use this regex, you should edit your nginx config file as shown in README.md
 regex = r"(?P<ipaddress>.+?) - - \[(?P<dateandtime>\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] \"(\w+) (?P<url>.+?(?=\ http\/1.1\")) http\/1.1\" \d{3} \d+ \"(?P<http_host>.+?(?=\"))\" (?P<http_user_agent>.+\s?(?=\ ))"
 lineformat = re.compile(regex, re.IGNORECASE)
+
+def average_value(list):
+    total = 0
+    for item in list:
+        total += item
+    return total / len(list)
 
 def parse_log_line(line):
     match = lineformat.match(line)
@@ -31,26 +47,28 @@ def parse_log_line(line):
     else:
         return None
 
-f = subprocess.Popen(['tail','-F',access_log],\
+localLogs = subprocess.Popen(localCmd,\
         stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-p = select.poll()
-p.register(f.stdout)
-# Pass top_urls to subproccess
-
+if remoteCmd:
+    remoteLogs = subprocess.Popen(remoteCmd,\
+        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+Poll = select.poll()
+Poll.register(localLogs.stdout)
+if remoteCmd:
+    Poll.register(remoteLogs.stdout)
 
 # Collect top 10 URLs in the last 5 minutes, print it and then drop cache
 top_urls = {}
+rps_history = []
 last_call = time.time()
+last_ddos_route = None
 
 def find_ddos_attack():
     # Determine if there is a ddos attack. Check if anomaly big count of requests to one endpoint (x10 times more than average)
     # If there is a ddos attack, deploy a firewall rule to block all traffic to this endpoint
-    global top_urls, rps, last_call
+    global top_urls, rps, last_call, last_ddos_route
 
     # Calculate rps
-    
-
-    
     top_20_urls = sorted(top_urls.items(), key=lambda x: x[1], reverse=True)[:20]
 
     # Calculate average
@@ -59,48 +77,63 @@ def find_ddos_attack():
         total += url[1]
     average = total / len(top_20_urls)
     rps = total / (time.time() - last_call)
-    print('RPS', rps)
-    print('Average', average)
+    rps_history.append(rps)
+    print(f'RPS: {rps} | Average RPS: {average_value(rps_history)} | Average: {average} | Total: {total} | Seconds since last stats: {time.time() - last_call}\n---------------------')
 
     # Check if there is a ddos attack
     for url in top_20_urls:
-        if url[1] > average * 10:
+        if url[1] > average * AVARAGE_COEFFICIENT or (average == url[1] and rps > RPS_MAX):
+            # Check if it is the same route as last time
+            if last_ddos_route == url[0]:
+                continue
             print('DDOS ATTACK DETECTED', url)
-
-            # Deploy firewall rule
+            cloudflare.run(url[0], 'challenge')
+            last_ddos_route = url[0]
 
 def print_top_urls():
     global top_urls
     # Sort by value, print top 10 (key, value) pairs
     sorted_urls = sorted(top_urls.items(), key=lambda x: x[1], reverse=True)
-    print(sorted_urls[:10])
-    print('---------------------')
+    # Print top 10 urls
+    for url in sorted_urls[:10]:
+        print('TOP > ',url[0], url[1])
+    print('--------------------- Time: ', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
     find_ddos_attack()
     top_urls = {}
 
-lines_scanned = 0
+
+def process_line(line):
+    global lines_scanned, top_urls
+    data = parse_log_line(line)
+    if data:
+        hostname = data['http_host']
+        url = data['url'].split("?")[0]
+        if any(route in url for route in routes_to_exclude):
+            return
+        ip = data['ipaddress']
+        full_url = hostname + url
+        print(ip, full_url, lines_scanned)
+        if full_url in top_urls:
+            top_urls[full_url] += 1
+        else:
+            top_urls[full_url] = 1
+    else:
+        print('No match line > ', line)
+    lines_scanned += 1
+
+
+    if(lines_scanned % SCAN_EVERY == 0):
+        print_top_urls()
+
+
 
 while True:
-    if p.poll(1):
-        line = str(f.stdout.readline().decode("utf-8"))
-        data = parse_log_line(line)
-        if data:
-            hostname = data['http_host']
-            url = data['url'].split("?")[0]
-            if any(route in url for route in routes_to_exclude):
-                # print('Excluded', url)
-                continue
-            ip = data['ipaddress']
-            full_url = hostname + url
-            print(ip, full_url, lines_scanned)
-            if full_url in top_urls:
-                top_urls[full_url] += 1
-            else:
-                top_urls[full_url] = 1
-        else:
-            print('No match line > ', line)
-        lines_scanned += 1
-
-
-        if(lines_scanned % 200 == 0):
-            print_top_urls()
+    if Poll.poll(1):
+        if lines_scanned == 0:
+            print('Start scanning logs')
+        if remoteLogs:
+            line = str(remoteLogs.stdout.readline().decode("utf-8"))
+            process_line(line)
+        if localLogs:
+            line2 = str(localLogs.stdout.readline().decode("utf-8")) 
+            process_line(line2)
--- cf.py
+++ cf.py
@@ -19,57 +19,62 @@ class Cloudflare:
 		for zone in response.json()['result']:
 			zones[zone['name']] = zone['id']
 		self.zones = zones
-		print('Loaded zones: ', zones)
+		print(f'[Cloudflare] Zones Loaded ({len(zones)})')
 		
 
-	def generate_rule (endpoint, action):
+	def generate_rule (self, endpoint, action):
 		# Endpoint can be: sub.domain.com/api/v1/endpoint
 		# Remove GET parameters, if any
 		endpoint = endpoint.split('?')[0]
-		# Remove trailing slash if any
-		#endpoint = endpoint.rstrip('/')
-		# Remove http(s)://
-		endpoint = endpoint.replace('http://', '').replace('https://', '')
+		endpoint = endpoint.split('/', 1)[1]
 
-		# Generate rule
+
 		rule = {
-			"match": {
-				"request": {
-					"methods": ["_ALL_"],
-					"schemes": ["_ALL_"],
-					"url": endpoint
-				},
-			},
-			"threshold": 10,
-			"period": 10,
-			"action": {
-				"mode": action,
-				"timeout": 10,
-			},
 			"description": "GeneratedRule",
-		};
-
-		return rule
+			#"expression": "(http.request.uri.path matches \"^/api/\")",
+			"expression": "(http.request.uri.path contains \""+endpoint+"\")",
+			"action": "block",
+			"ratelimit": {
+				"characteristics": [
+					"ip.src",
+					"cf.colo.id"
+				],
+				"period": 60,
+				"requests_per_period": 100,
+				"mitigation_timeout": 600
+			},
+			"action_parameters": {
+				"response": {
+					"status_code": 404,
+					"content": "{\"success\": false, \"error\": \"Sorry, our website is under DDOS attack by some assholes. Automatic system detects your activity as bot, just wait a little and try again! Sorry!\"}",
+					"content_type": "application/json"
+				}
+			}
+		}
+		print('Generated rule: ', rule)
+		return [rule]
 
 	def get_existing_rules (self, zone_id):
-		url = 'https://api.cloudflare.com/client/v4/zones/' + zone_id + '/rate_limits'	
+		url = 'https://api.cloudflare.com/client/v4/zones/' + zone_id + '/rulesets/phases/http_ratelimit/entrypoint';
 		response = requests.get(url, headers=self.headers)
-		return rules.json()
+		print(f'[{zone_id}] Existing rules: ', response.json())
+		return response.json()
 
 	def delete_rule (self, rule_id, zone_id):
-		url = 'https://api.cloudflare.com/client/v4/zones/' + zone_id + '/rate_limits/' + rule_id
+		print(f'[{zone_id}] Deleting rule: ', rule_id)
+		url = 'https://api.cloudflare.com/client/v4/zones/' + zone_id + '/rulesets/' + rule_id
 		response = requests.delete(url, headers=self.headers)
 		return response.json()
 
 	def delete_all_generated_rules (self, zone_id):
 		rules = self.get_existing_rules(zone_id)
-		for rule in rules['result']:
+		for rule in rules['result']['rules']:
 			if rule['description'] == 'GeneratedRule':
 				self.delete_rule(rule['id'], zone_id)
 
 	def add_rule (self, rule, zone_id):
-		url = 'https://api.cloudflare.com/client/v4/zones/' + zone_id + '/rate_limits'
-		response = requests.post(url, headers=self.headers, json=rule)
+		url = 'https://api.cloudflare.com/client/v4/zones/' + zone_id + '/rulesets/phases/http_ratelimit/entrypoint'
+		response = requests.put(url, headers=self.headers, json=rule)
 		return response.json()
 
 	def run (self, endpoint, action):
@@ -82,7 +87,11 @@ class Cloudflare:
 		if zone_id is None:
 			print('Zone for endpoint not found', endpoint)
 			return False
+		else:
+			print('Zone for endpoint found', zone_id)
 
 		rule = self.generate_rule(endpoint, action)
+		print(f'[{zone_id}] Generated rule: ', rule) 
 		self.delete_all_generated_rules(zone_id)
-		self.add_rule(rule, zone_id)
+		result = self.add_rule(rule, zone_id)
+		print(f'[{zone_id}] Result: ', result)
